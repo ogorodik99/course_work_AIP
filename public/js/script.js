0a0a072ab1e75dc6c5864f006e898e485f3dfb8f
ogorodik99 <ogorodik99@yandex.ru>
2025-11-29 17:56:09 +0300

    главная страница

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".card-item");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    cards.forEach(card => observer.observe(card));
+});
